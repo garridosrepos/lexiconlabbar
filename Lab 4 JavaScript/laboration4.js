fdf7556e7adde7573a779aa1fd511d84c4bfde02
Ivan Garrido <ivangarrido@icloud.com>
2022-09-21 18:47:05 +0200

    Lab 4 klart

---
+++ Lab 4 JavaScript/laboration4.js
@@ -0,0 +1,65 @@
+
+let multiplication = {
+
+    calculate(x,y){
+        return x * y;
+    },
+
+    stringify(x, y){
+        return " "+ x + " * " + y +":  " + this.calculate(x,y);
+    }
+}
+
+let subtraction = {
+
+    calculate: function(x, y){
+        return x - y;
+    },
+
+    stringify: function(x,y){
+        return " "+ x + " - " + y +":  " + this.calculate(x,y);
+    }
+}
+let addition = {
+
+    calculate: function(x,y){
+        return x + y;
+    },
+
+    stringify: function(x,y){
+        return " "+ x + " + " + y +":  " + this.calculate(x,y);
+    }
+}
+
+
+
+
+let person = {
+    firstName: "Bengan",
+    lastName: "Bengtsson",
+    age: 43,
+};
+
+
+
+
+/***** MAIN *****/
+
+let x = 10;
+let y = 20;
+console.log("Del 1:\nDeklarerade variabler: x="+ x + " och y="+ y + "\n");
+
+console.log("Addition ger:  "+ addition.calculate(x,y));
+console.log("Subtraktion ger: "+ subtraction.calculate(x, y));
+console.log("Multiplikation:  " + multiplication.calculate(x, y));
+
+
+console.log("Del 2");
+console.log(addition.stringify( x, y));
+console.log(subtraction.stringify( x, y));
+console.log(multiplication.stringify( x, y));
+
+
+
+console.log("Hej, mitt namn är "+ person.firstName + " "+ person.lastName + " och jag är "+ person.age + " år gammal.");
+
