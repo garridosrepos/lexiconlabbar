d123dec4a57899cd9198fc1ec9d826f74a5f0c61
Ivan Garrido <ivangarrido@icloud.com>
2022-09-26 11:20:35 +0200

    setting up lab 6

---
+++ Lab 6/functions.js
@@ -0,0 +1,34 @@
+
+
+/*Funktion 1:*/
+let hello= () => console.log("Hello world");
+
+
+/** Funktion 2:*/
+
+let greet = (name) => console.log('Hello '+name + 'how are you doing?');
+
+
+/*
+
+Funktion 3:
+
+const calc = function(numberOne, numberTwo) {
+    return numberOne + numberTwo;
+}
+*/
+
+
+/*
+Funktion 4:
+
+const tip = function(sum, percent) {
+    let total = sum + sum * percent;return total;
+}
+*/
+
+let v = (x,y)=>x+y;
+
+console.log(v(8,9));
+
+
