bcc8eb425cd216b48d466ca47c08b01ac3eda4d7
Ivan Garrido <ivangarrido@icloud.com>
2022-09-27 15:54:17 +0200

    LEXICON-7 Lab 7 klar. Några små grejer som inte blev som det skulle kanske
    - avrundning av resultatet.
    - hantering av negativa tal.
    - style på output

--- Lab 7/3/script.js
+++ Lab 7/3/script.js
@@ -2,16 +2,43 @@
 
 
 
-let changeDestination = function(){
-    let anchor = document.querySelector("#anchor");
-    anchor.innerHTML = "Go to Google";
-    anchor.setAttribute("href", "https://www.google.se");
-    console.log(anchor.attributes);
+let width = document.querySelector( '#cube-width.form-control');
+let height = document.querySelector('#cube-height.form-control');
+let depth = document.querySelector( '#cube-depth.form-control');
+let result = document.querySelector("#result")
+let volResult = document.querySelector("#volResult")
+
+let volume = function () {
+    var value = parseFloat(width.value) * parseFloat(height.value) * parseFloat(depth.value);
+    value = Math.fround(value);
+
+    console.log("Calculated volume-value");
+    if(parseFloat(width.value) < 0 || parseFloat(height.value) < 0 ||parseFloat(depth.value)< 0){
+        value = value * -1;
+    }
+    return value;
 }
 
 
-let button = document.querySelector(".changer")
+let updateVolume = function () {
+    console.log(this.id + ": " + this.value);
+    let vol = volume();
+    result.innerHTML = vol;
+    volResult.innerHTML = vol;
+    /*console.log(this);*/
 
-button.addEventListener('click', changeDestination);
+}
+
+updateVolume();
+/*
+width.addEventListener('valueChange', updateVolume);
+height.addEventListener('click', updateVolume);
+depth.addEventListener('click', updateVolume);
+*/
+
+
+
+
+
 
 
