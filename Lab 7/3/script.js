1538150570e2e3bee8a485689c8fcf4c4b83957f
Ivan Garrido <ivangarrido@icloud.com>
2022-09-27 17:12:35 +0200

    LEXICON-7 Lab 7 klar. Några små grejer som inte blev som det skulle kanske
    - avrundning av resultatet.
    - hantering av negativa tal.
    - style på output
    
    cleaning up

--- Lab 7/3/script.js
+++ Lab 7/3/script.js
@@ -2,11 +2,11 @@
 
 
 
-let width = document.querySelector( '#cube-width.form-control');
-let height = document.querySelector('#cube-height.form-control');
-let depth = document.querySelector( '#cube-depth.form-control');
-let result = document.querySelector("#result")
-let volResult = document.querySelector("#volResult")
+let width       = document.querySelector('#cube-width.form-control');
+let height      = document.querySelector('#cube-height.form-control');
+let depth       = document.querySelector('#cube-depth.form-control');
+let result      = document.querySelector("#result")
+let volResult   = document.querySelector("#volResult")
 
 let volume = function () {
 
@@ -15,11 +15,7 @@ let volume = function () {
     let d = parseFloat(depth.value);
 
     var value = h * d * w;
-
-
-
     var volumeValue = Math.fround(value);
-
 
     console.log("Calculated volume-value: "+ volumeValue);
 
@@ -27,6 +23,7 @@ let volume = function () {
         volumeValue = volumeValue * -1;
     }
     return volumeValue;
+
 }
 
 
